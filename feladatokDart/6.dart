fd700965c60f574e7bcb919195b2434718a31604
nigabiga <alexmarjanovic64@gmail.com>
2026-09-22 09:01:55 +0200

    Refactor game logic to correctly determine winner based on player inputs

--- feladatokDart/6.dart
+++ feladatokDart/6.dart
@@ -5,8 +5,20 @@ void main() {
   int? masodik = int.tryParse(stdin.readLineSync()!);
 
   if (elso != null && masodik != null) {
-    if ((elso - masodik).abs() == 2) {
+    if ((elso - masodik).abs() == 0) {
       print("Dontetlen");
+    } else if (elso == 1 && masodik == 2) {
+      print("Player 2 wins");
+    } else if (elso == 1 && masodik == 3) {
+      print("Player 1 wins");
+    } else if (elso == 2 && masodik == 1) {
+      print("Player 1 wins");
+    } else if (elso == 2 && masodik == 3) {
+      print("Player 2 wins");
+    } else if (elso == 3 && masodik == 1) {
+      print("Player 2 wins");
+    } else if (elso == 3 && masodik == 2) {
+      print("Player 1 wins");
     }
   }
 }
